e59f1e0784e5cbf713c025a176180a5b0fe821a0
bianhua <15733197761@163.com>
2018-04-26 10:57:09 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import {Provider} from 'react-redux'
+import {Provider} from 'react-redux'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-// import store from './redux/store'
 
 import Home from './containers/home/home';
 import Category from './containers/category/category';
 import Shopping from './containers/shopping/shopping';
 import User from './containers/user/user';
+import store from './redux/store'
 
 ReactDOM.render((
-  <BrowserRouter>
-    <Switch>
-      <Route path='/category' component={Category}/>
-      <Route path='/shopping' component={Shopping}/>
-      <Route path='/user' component={User}/>
-      <Route component={Home}/> {/*默认路由组件*/}
-    </Switch>
-  </BrowserRouter>
+  <Provider store = {store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/category' component={Category}/>
+        <Route path='/shopping' component={Shopping}/>
+        <Route path='/user' component={User}/>
+        <Route component={Home}/> {/*默认路由组件*/}
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
